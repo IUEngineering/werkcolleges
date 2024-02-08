eadc150d3823aef9394f2ebc32c984f72abac78c
illyau <illya@iuengineering.com>
2024-02-09 00:16:34 +0100

    4b not done

---
+++ wc04/04.b/compiler.cpp
@@ -0,0 +1,105 @@
+// 3.c runtime
+//
+// g++ -Wall -o runtime compiler.cpp parser.cpp runtime.cpp
+// ./runtime
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.hpp"
+#include "runtime.hpp"
+
+#define MAX_INVOER 100
+
+int main(int nArgc, char* aArgv[]) {
+
+  Parser oParser;
+  Runtime oRuntime;
+
+  char sBuffer[MAX_INVOER + 1];
+  uint8_t nCount;
+
+  // Initialiseren
+  printf("03.c runtime\n\n");
+  
+  // Vraag om een regel tekst in sBuffer
+  do
+  {
+    // Lees een regel
+    printf("\nCommandoinvoer: ");
+    fgets(sBuffer, MAX_INVOER, stdin);
+    printf("Gelezen: [%s]\n", sBuffer);
+
+    // Parse de regel in tokens
+    nCount = oParser.parse(sBuffer);
+
+    /*// Toon de tokens
+    printf("Tokens %d: ", nCount);
+    for (uint8_t i = 0; i < nCount; i++) {
+      printf("{%s}", oParser.token(i));
+    };
+    printf("\n");
+    */
+
+    // Voer de ingelezen commando's uit
+    if (nCount > 0) {
+      if (strcmp(oParser.token(0), "knop") == 0) {
+        // Goed commando?
+        if (nCount == 2) {
+          bool bRet = oRuntime.knop(oParser.token(1));
+        }
+        else {
+          printf(" Syntax error! ");
+        };
+      }
+      else
+      if (strcmp(oParser.token(0), "led") == 0) {
+        // Goed commando?
+        if (nCount == 3) {
+          oRuntime.led(oParser.token(1), strcmp(oParser.token(2), "on") == 0);
+        }
+        else {
+          printf(" Syntax error! ");
+        };
+      }
+      else
+      if (strcmp(oParser.token(0), "label") == 0) {
+        // Goed commando?
+        if (nCount == 2) {
+          oRuntime.label(oParser.token(1));
+        }
+        else {
+          printf(" Syntax error! ");
+        };
+      }
+      else
+      if (strcmp(oParser.token(0), "jump") == 0) {
+        // Goed commando?
+        if (nCount == 2) {
+          oRuntime.jump(oParser.token(1));
+        }
+        else {
+          printf(" Syntax error! ");
+        };
+      }
+
+      else
+      if (strcmp(oParser.token(0), "wait") == 0) {
+        // Goed commando?
+        if (nCount == 2) {
+          oRuntime.wait(atoi(oParser.token(1)));
+        }
+        else {
+          printf(" Syntax error! ");
+        };
+      }
+      else {
+        printf(" Onbekende invoer! ");
+      };
+    };
+
+  } while (strcmp(sBuffer, "\n") != 0);
+
+  // Afsluiten
+  printf("\nAfgesloten :-)\n\n");
+  return 0;
+};
---
+++ wc04/04.b/parser.cpp
@@ -0,0 +1,55 @@
+// 3.b parser.cpp
+
+#include <cstring>
+#include "parser.hpp"
+
+Parser::Parser() {
+
+  // Maak de tokens leeg
+  memset(aToken_, 0, PARSER_MAX_TOKENS * PARSER_MAX_TOKENSSIZE);
+};
+
+Parser::~Parser() {
+
+};
+
+uint8_t Parser::parse(char* sLine) {
+
+  uint8_t nTokens = 0;
+  uint16_t nIndex = 0, t = 0;
+  
+  // Loop alle tekens langs tot \0
+  while (sLine[nIndex] != 0) {
+
+    // Sla witruimte over: tab, spatie en enter
+    //TODO: zorg dat de spatie ook als witruimte wordt gezien
+    while (sLine[nIndex] > 0 && (sLine[nIndex] == ' ' || sLine[nIndex] == '\t'|| sLine[nIndex] == '\n' || sLine[nIndex] == '\r')) {
+      nIndex++;
+    };
+
+    // Lees en sla op tot het einde van sLine of tot de eerste witruimte
+    t = 0;
+    //TODO: zorg dat de spatie ook als witruimte wordt gezien
+    while (sLine[nIndex] > 0 && sLine[nIndex] != '\t' && sLine[nIndex] != '\n' && sLine[nIndex] != '\r' && sLine[nIndex] != ' ') {
+      aToken_[nTokens][t] = sLine[nIndex];
+      t++; nIndex++;
+    };
+
+    // Echt een token gelezen?
+    if (t > 0) {
+      // Sluit de string af
+      aToken_[nTokens][t] = 0;
+      nTokens++;
+    };
+  };
+
+  return nTokens;
+};
+
+char* Parser::token(uint8_t nIndex) {
+
+  if (nIndex >= PARSER_MAX_TOKENS) {
+    nIndex = 0;
+  };
+  return aToken_[nIndex];
+};
